fb643c646a6a2c52a7b451a4bf12568f03440df5
Dev Halai <Dev.h@hotmail.co.uk>
2024-06-20 00:23:47 +0100

    weapons and battle logic

--- logic/level.py
+++ logic/level.py
@@ -3,4 +3,9 @@ import math
 def calculate_level(xp):
     return int(math.floor(xp/10))
 
-print(calculate_level(78))
+def set_stats(knight):
+    # Destructure values in dictionary of player stats
+    level, xp, max_health, current_health, attack, defence = knight.values()
+    base_value = int(10)
+    attack = defence = int(base_value * level)
+    max_health = int(100) + base_value * level
