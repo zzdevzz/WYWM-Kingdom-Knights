b7b195bb311df47a695846a3c2e1ed1340babc05
Dev Halai <Dev.h@hotmail.co.uk>
2024-06-26 01:29:27 +0100

    style and final

--- logic/level.py
+++ logic/level.py
@@ -1,11 +1,22 @@
 import math
 
 def calculate_level(xp):
+    
+    """Set level based on current xp"""
     return int(math.floor(xp/10))
 
 def set_stats(knight):
-    # Destructure values in dictionary of player stats
-    level, xp, health, attack, defence = knight.values()
+    
+    """Set and update knight's stats based on the level."""
+    
     base_value = int(10)
-    attack = defence = int(base_value * level)
-    max_health = int(100) + base_value * level
+    level = knight["stats"]["level"]
+    
+    # Calculate new stats
+    attack = defence = base_value * level
+    max_health = 100 + base_value * level
+
+    # Update the knight's stats in the dictionary
+    knight["stats"]["attack"] = attack
+    knight["stats"]["defence"] = defence
+    knight["stats"]["max_health"] = max_health
